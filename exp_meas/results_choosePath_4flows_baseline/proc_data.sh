47aba23ae3b1bb9c95778568b48293d236b941a4
giditre <giditre@gmail.com>
2020-09-21 19:52:21 +0200

    commit from ubunto on 21/09/2020 at 19:52

---
+++ exp_meas/results_choosePath_4flows_baseline/proc_data.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+infname=$1
+
+if ! [[ $infname ]] ; then
+  echo "Must provide an input file name."
+  exit 1
+fi
+
+if [[ $2 ]] ; then
+  outfname=$2
+else
+  outfname=procd_$infname
+fi
+
+[[ -f $outfname ]] && rm $outfname
+
+inittime=$(head -1 $infname | cut -d' ' -f1)
+prevvalue=$(head -1 $infname | cut -d' ' -f2)
+
+cat $infname | tail -n +1 | while read line; do
+  currtime=$(echo $line | cut -d' ' -f1)
+  currvalue=$(echo $line | cut -d' ' -f2)
+  printf "%3d %15d\n" "$(( $currtime - $inittime ))" "$(( 8*($currvalue - $prevvalue) ))" | tee -a $outfname
+  prevvalue=$currvalue
+done
+
